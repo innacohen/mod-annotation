77f0d25509a3c9e6b037e0611331920340fa07d2
Inna Cohen <inessa.cohen@yale.edu>
2026-04-23 04:47:29 -0400

    repo restructuring

---
+++ code/3-1table1.R
@@ -0,0 +1,25 @@
+source("code/_utils.R")
+pred_df <- read_csv("data/pipeline/predictions.csv")
+
+
+# TABLE 1 -----------------------------------------------------------------
+
+ant_df = read_csv("data/pipeline/split_df2_with_labels.csv") %>%  
+  mutate(set = factor(set, levels=c("train","val","test"), labels=c("Train","Validation","Test")))
+
+
+label(ant_df$type) = "Type"
+label(ant_df$label) = "Subtype"
+
+
+t1 = table1(~type+label |set, data=ant_df, overall=F, extra.col=list(`P-value`=pvalue))
+
+# list of tables and the doc
+my_list <- list(df1 <- t1flex(t1))
+
+#my_doc <- read_docx()
+#walk(my_list, write_word_table, my_doc) 
+#fname = paste0("../output/mod_file_tables_",Sys.Date(),".docx")
+#dir.create(dirname(fname), showWarnings = FALSE, recursive = TRUE)
+#print(my_doc, target = fname) %>% invisible()
+
